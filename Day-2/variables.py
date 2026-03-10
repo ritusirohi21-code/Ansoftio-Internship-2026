10b0f74629e99457311a9b1043d024f99b1a7c4d
ritusirohi21-code <sirohiritu22@gmail.com>
2026-03-10 20:12:07 +0530

    Create variables.py
    Python program demonstrating variables and data types.

---
+++ Day-2/variables.py
@@ -0,0 +1,9 @@
+student_name = "Ritu Sirohi"
+age = 22
+city = "Ahilyanagar"
+course = "Python Developer Internship"
+
+print("Student Name:", student_name)
+print("Age:", age)
+print("City:", city)
+print("Course:", course)
