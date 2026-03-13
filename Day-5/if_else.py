0b7ccba4bfb7eebf13a97914dde51cd8db3b3a02
ritusirohi21-code <sirohiritu22@gmail.com>
2026-03-13 19:55:41 +0530

    conditional statements program

---
+++ Day-5/if_else.py
@@ -0,0 +1,8 @@
+# Voting Eligibility Program
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Congratulations! You are eligible to vote.")
+else:
+    print("Sorry, you are not eligible to vote yet.")
