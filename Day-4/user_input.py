784009e0d266fea9aa5a21a366637041047b89a8
ritusirohi21-code <sirohiritu22@gmail.com>
2026-03-13 19:40:24 +0530

    Add Day-4 user input program

---
+++ Day-4/user_input.py
@@ -0,0 +1,12 @@
+# Student Information Program
+
+name = input("Enter student name: ")
+age = input("Enter age: ")
+city = input("Enter city: ")
+course = input("Enter course: ")
+
+print("\nStudent Information")
+print("Student Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Course:", course)
