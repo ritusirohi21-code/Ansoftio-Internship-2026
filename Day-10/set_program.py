11fca1f185cf0f53ed37a71aff05d4fd374d1407
ritusirohi21-code <sirohiritu22@gmail.com>
2026-04-01 12:38:29 +0530

    Set Program

---
+++ Day-10/set_program.py
@@ -0,0 +1,11 @@
+# Python Developer Internship – Day 10
+# Topic: Sets in Python
+
+# List with duplicate values
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+# Converting list into set
+unique_numbers = set(numbers)
+
+# Printing only unique values
+print("Unique values:", unique_numbers)
