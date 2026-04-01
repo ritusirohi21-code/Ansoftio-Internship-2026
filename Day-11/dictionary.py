7228a6f2d026869d7b3f8882d2e5d0834d5cdb2f
ritusirohi21-code <sirohiritu22@gmail.com>
2026-04-01 12:41:25 +0530

    Dictionary program

---
+++ Day-11/dictionary.py
@@ -0,0 +1,14 @@
+# Python Developer Internship – Day 11
+# Topic: Dictionaries in Python
+
+student = {
+    "name": "Marc",
+    "age": 22,
+    "city": "Pune",
+    "course": "MCA"
+}
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("City:", student["city"])
+print("Course:", student["course"])
